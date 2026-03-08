fa2b76b85232b167988916bfd7f830c300cd41f1
Devendra Padala <devendrapadala25@gmail.com>
2026-03-08 10:47:25 +0530

    added

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,13 +2,13 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEy = '29c4d2a018dee2ff27a0c7dc544374bd';
 
 export const getMovies = async () => {
-    const res = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEy}`)
+    const res = await fetch(`/api/tmdb?path=movie/popular`)
     const data = await res.json();
     return data.results;
 }
 
 export const searchMovie = async (query) => {
-    const res = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEy}&query=${encodeURIComponent(query)}`)
+    const res = await  fetch(`/api/tmdb?path=search/movie&query=${encodeURIComponent(query)}`)
     const data = await res.json();
     return data.results;
 }
